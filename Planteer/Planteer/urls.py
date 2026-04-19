40e55564595aa605d73064a752fc7a85a1c2e1c0
Rimas Alshahrani <remasmushbab@gmail.com>
2026-04-19 19:37:45 +0300

    add bonus contact section and polish UI designs

--- Planteer/Planteer/urls.py
+++ Planteer/Planteer/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),             
-    path('plants/', include("plants.urls")),   
+    path('plants/', include("plants.urls")), 
+    path('contact/', include('contact.urls')), 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
